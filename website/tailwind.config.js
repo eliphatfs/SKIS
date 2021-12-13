f70ac9df7494e4ce4b0530b472fe2c03ca69376a
eliphatfs <flandre@scarletx.cn>
2021-12-13 23:20:42 +0800

    Setup website project.

---
+++ website/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ["./src/*.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
